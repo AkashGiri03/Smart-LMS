16660c8ac10f2c2451b7ca09c9c853a0b43252ad
AkashGiri03 <113370221+AkashGiri03@users.noreply.github.com>
2026-01-29 19:31:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Navbar from "./components/layouts/Navbar.jsx";
 import Footer from "./components/layouts/Footer.jsx";
-import Contact from "./components/layouts/ContactUs.jsx;
+import Contact from "./components/layouts/ContactUs.jsx";
 import CoursesPage from "./pages/CoursesPage.jsx";
 import CourseDetails from "./pages/CourseDetails.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
